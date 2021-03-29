2caaff6ea9fb503de9f40a00243e380211af29ca
bikashsahoo1636 <bikashsahoo779@gmail.com>
2021-03-29 19:47:59 +0530

    added the watchlist and wishlist

--- MovieVault - source code/user_movie.js
+++ MovieVault - source code/user_movie.js
@@ -3,9 +3,10 @@ module.exports = (function () {
     var router = express.Router();
   
     function getWishList(res, mysql, context, complete, id) {
+        console.log(res);
       // var sql = "SELECT movie_title, score, explanation, ratingID From Rating_Entry Inner Join Movie on Rating_Entry.movie_id=Movie.movie_id WHERE account_ID = ?";
       var sql =
-        "SELECT Movie.movie_title FROM User_Movie_Detail JOIN Movie ON Movie.movie_id = User_Movie_Detail.movie_id AND User_Movie_Detail.user_id = ? and in_wishList=1";
+        "SELECT Movie.movie_title FROM User_Movie_Detail JOIN Movie ON Movie.movie_id = User_Movie_Detail.movie_id AND User_Movie_Detail.user_id = ? and User_Movie_Detail.in_wishList=1";
       var inserts = [id];
       mysql.pool.query(sql, inserts, function (error, results, fields) {
         if (error) {
@@ -21,7 +22,7 @@ module.exports = (function () {
     function getWatchList(res, mysql, context, complete, id) {
         // var sql = "SELECT movie_title, score, explanation, ratingID From Rating_Entry Inner Join Movie on Rating_Entry.movie_id=Movie.movie_id WHERE account_ID = ?";
         var sql =
-          "SELECT Movie.movie_title FROM User_Movie_Detail JOIN Movie ON Movie.movie_id = User_Movie_Detail.movie_id AND User_Movie_Detail.user_id = ? and is_watched=1";
+          "SELECT Movie.movie_title FROM User_Movie_Detail JOIN Movie ON Movie.movie_id = User_Movie_Detail.movie_id AND User_Movie_Detail.user_id = ? and User_Movie_Detail.is_watched=1";
         var inserts = [id];
         mysql.pool.query(sql, inserts, function (error, results, fields) {
           if (error) {
@@ -58,25 +59,23 @@ module.exports = (function () {
   
     router.get("/", function (req, res) {
       currAccount = req.session.username;
-  
-      console.log("profile: " + req.session.loggedin);
+    
+      console.log("user_movie: " + req.session.loggedin);
   
       if (req.session.loggedin != true) {
-        res.redirect("/login");
+        return res.redirect("/login");
       } else {
         var context = {};
         var callbackCount = 0;
-        context.jsscripts = ["delete_rating.js", "updaterating.js"];
         var mysql = req.app.get("mysql");
-  
         getUsername(res, mysql, context, complete, currAccount);
         getWishList(res, mysql, context, complete, currAccount);
         getWatchList(res, mysql, context, complete, currAccount);
         function complete() {
           callbackCount++;
-          if (callbackCount >= 2) {
+          if (callbackCount >= 3) {
             console.log(context.username);
-            res.render("user_movie", context);
+            return res.render("user_movie", context);
           }
         }
       }
