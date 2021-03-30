015993f5d659ae1f5c5753ba875cfbac6f76557a
Rohan Meena <rohanmeena07@iitkgp.ac.in>
2021-03-30 09:38:48 +0530

    imdb , rotten tomatoes and user watched added

--- MovieVault - source code/dbcon.js
+++ MovieVault - source code/dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'localhost',
-  user            : 'root',
-  password        : 'bottletopple202',
-  database        : 'movie_new'
+  user            : 'h2s',
+  password        : 'password',
+  database        : 'database_something'
 });
 module.exports.pool = pool;
